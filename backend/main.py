e2ad682c2ed77616ebf16e90c41a119449408aa4
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-06-13 00:10:17 +0700

    refactor: update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,13 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Session
 
-app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    desc: str
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.post("/")
-def creat_item(item: Item):
-    return {"Item created" : item}
+from database.db import engine
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+
+app = FastAPI(lifespan=lifespan)
+

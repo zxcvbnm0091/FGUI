85f6b6d752c992c8f29216d6045ab1bbc101ddac
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-06-13 00:10:34 +0700

    feat: add get  session

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,6 +1,11 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
+
 
 database_filename="../data/game.db"
 database_url = f"sqlite:///{database_filename}"
 
 engine = create_engine(database_url)
+
+def get_session():
+    with Session(engine) as session:
+        yield session

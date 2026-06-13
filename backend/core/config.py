e3abe5031e421144057e819e0c30526af77449c8
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-06-14 00:11:59 +0700

    refactor: remove and change deprecated library

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,6 @@
 import secrets
 from typing import Optional, Dict, Any
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"

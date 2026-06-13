0641025003cdd31051f7b1f91ad2a43f0f3fbe62
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-06-14 00:15:14 +0700

    chore: correct typo

--- backend/models/wishlist.py
+++ backend/models/wishlist.py
@@ -9,7 +9,7 @@ class WishlistGame(SQLModel, table=True):
     __table_args__ = (UniqueConstraint("user_id", "igdb_game_id"),)
 
     id: str = Field(default_factory=generate_cuid, primary_key=True)
-    user_id: str = Field(foreign_key="user.id", nullable=False, index=True)
+    user_id: str = Field(foreign_key="users.id", nullable=False, index=True)
     igdb_game_id: int = Field(foreign_key="igdb_games.id", nullable=False, index=True)
     matched_game_id: Optional[int] = Field(default=None, foreign_key="games.id")
     notified: bool = Field(default=False)

9545aaa6e97a058f8268ac88d2daf5ce7340ce15
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-06-14 00:14:07 +0700

    refactor: fix incorrect field

--- backend/models/game.py
+++ backend/models/game.py
@@ -1,24 +1,26 @@
 from datetime import datetime, timezone
-from typing import Optional
+from typing import Optional, List
 from sqlmodel import SQLModel, Field
 
-class Game(SQLModel):
-    id: int = Field(primary_key=True)
+class Game(SQLModel, table=True):
+    __tablename__="games"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     url: str = Field(unique=True)
     date: Optional[str] = None
     repack_number: Optional[str] = None
     version: Optional[str] = None
     cover_image: Optional[str] = None
-    genres: Optional[str] = None
+    genres: Optional[List[str]] = None
     companies: Optional[str] = None
-    language: Optional[str] = None
+    languages: Optional[str] = None
     original_size: Optional[str] = None
     repack_size: Optional[str] = None
-    screenshots: Optional[str] = None
+    screenshots: Optional[List[str]] = None
     trailer: Optional[str] = None
-    direct_mirrors: Optional[str] = None
-    torrent_mirrors: Optional[str] = None
-    repack_features: Optional[str] = None
+    direct_mirrors: Optional[List[str]] = None
+    torrent_mirrors: Optional[List[str]] = None
+    repack_features: Optional[List[str]] = None
     description: Optional[str] = None
-    scrape_at: datetime
+    scraped_at: datetime

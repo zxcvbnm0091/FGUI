b7fcdaacbdad56d4b17369e1dff8bed18a01162f
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-06-13 22:12:54 +0700

    feat: add get admin

--- backend/dependencies/auth.py
+++ backend/dependencies/auth.py
@@ -17,3 +17,27 @@ def get_current_user(session_id: str = Cookie(None), session: Session = Depends(
             status_code=status.HTTP_401_UNAUTHORIZED, details="Invalid session cookie")
     
     return user
+
+def get_current_admin(session_id: str = Cookie(None), session: Session = Depends(get_session)) -> User:
+    if not session_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            details="Not authorized. Missing sesison cookie"
+        )
+    
+    admin = session.get(User, session_id)
+
+    if not admin:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            details="Invalid cookie session"
+        )
+    
+    if admin.role != "ADMIN":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            details="Not authorized"
+        )
+    
+    return admin
+

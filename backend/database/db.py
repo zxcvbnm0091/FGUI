438e46a9011ae9225e71510e9dd84c8e12a9784d
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-06-14 00:12:37 +0700

    feat: add path for database url

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,10 +1,14 @@
-from sqlmodel import create_engine, Session
+from sqlmodel import create_engine, Session, text
+from pathlib import Path
 
-
-database_filename="../data/game.db"
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+database_filename = BASE_DIR / "data" / "games.db"
 database_url = f"sqlite:///{database_filename}"
 
-engine = create_engine(database_url)
+engine = create_engine(database_url, connect_args={"check_same_thread": False})
+
+with engine.connect() as conn:
+    conn.execute(text("PRAGMA journal_mode=WAL"))
 
 def get_session():
     with Session(engine) as session:

72de3d6c2a31456bef7251d23da461c388449b0e
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-06-13 22:14:43 +0700

    feat: add user service

---
+++ backend/services/user.py
@@ -0,0 +1,7 @@
+from typing import Optional
+from sqlmodel import Session, select
+from models.user import User
+
+def get_user_by_email(session: Session, email: str) -> Optional[User]:
+    statement = select(User).where(User.email == email)
+    return session.exec(statement).first()
